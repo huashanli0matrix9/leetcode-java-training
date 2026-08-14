87221c7aee6b69abf02733eb8b3fab44ad8d6335
Huashan.Li <huashanli0matrix9@gmail.com>
2026-08-14 05:14:50 +0000

    Solve Two Sum with HashMap

--- drafts/0001-two-sum/src/main/java/Solution.java
+++ drafts/0001-two-sum/src/main/java/Solution.java
@@ -1,10 +1,41 @@
+import java.util.HashMap;
+
 public class Solution {
     public static int[] twoSum(int[] nums, int target) {
-        // TODO: implement your solution
+
+        //Map solution: time O(n) - space O(n)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+            System.out.println(map);
+        }
+        
+        /* time O(n^2) - space O(1) solution 
+        if (nums==null || nums.length==0) {
+            return new int[0];
+        } else {
+            for (int i=0; i<nums.length; i++) {
+                for (int j = i+1; j<nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i,j};
+                    }
+                }
+            }
+        } 
+        
+        */
         return new int[0];
     }
 
     public static void main(String[] args) {
-        // TODO: add the three examples from the task
+        
+        int [] nums = {-1, -2, 0, 4, 5};
+        int target = 3;
+        int[] result = twoSum(nums, target);
+        System.out.println("Test case 1: Indices of the two numbers that add up to " + target + ": [" + result[0] + ", " + result[1] + "]");
     }
 }
